0538db172a0561deabb2e393818677452baec541
jasonalmaturner <jasonalmaturner@gmail.com>
2014-10-01 17:34:33 -0600

    finished basic functions. Needs styling and some finess

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,13 +1,38 @@
 var app = angular.module('parseQ');
 
 app.controller('mainController', function($scope, parseService){
-  $scope.test = "what what";
 
-  $scope.postData = function(){
-  	var question = {};
-  	question.text = $scope.question;
-  	parseService.postQuestion(question);
-  	$scope.question = "";
-  }
+$scope.test = "what what";
+
+$scope.postData = function(){
+  var question = {};
+  question.text = $scope.question;
+  question.status = 'red';
+  parseService.postQuestion(question);
+  $scope.question = "";
+  $scope.getParseData();
+}
+
+$scope.getParseData = function(){
+  parseService.getData().then(function(data){
+  	$scope.questions = data.data.results;
+  	console.log($scope.questions);
+  })
+}
+
+$scope.changeStatus = function(key){
+	key.status = 'yellow';
+	parseService.updateData(key).then(function(){
+		$scope.getParseData();
+	})
+};
+
+$scope.deleteData = function(key){
+	parseService.deleteData(key).then(function(){
+		$scope.getParseData();
+	})
+}
+
+$scope.getParseData();
 
 })
--- js/services/parseService.js
+++ js/services/parseService.js
@@ -2,14 +2,37 @@ var app = angular.module('parseQ');
 
 app.service('parseService', function($http){
 
-  this.postQuestion = function(question){
-    return $http({
-	  method: 'POST',
-	  data: question,
-	  url: 'https://api.parse.com/1/classes/queue'
-	  })/*.then(function(data){
-	    deferred.resolve(data.data.results);
-	    });*/
-	}
+this.postQuestion = function(question){
+  return $http({
+    method: 'POST',
+    data: question,
+    url: 'https://api.parse.com/1/classes/queue'
+  })/*.then(function(data){
+      deferred.resolve(data.data.results);
+      });*/
+};
 
-})
+this.getData = function(){
+  return $http({
+    method: 'GET',
+    url: 'https://api.parse.com/1/classes/queue'
+  })
+};
+
+this.updateData = function(question){
+  return $http({
+    method: 'PUT',
+    data: question,
+    url: 'https://api.parse.com/1/classes/queue/' + question.objectId
+  })
+};
+
+this.deleteData = function(question){
+  return $http({
+    method: 'DELETE',
+    data: question,
+    url: 'https://api.parse.com/1/classes/queue/' + question.objectId
+  })
+}
+
+});
